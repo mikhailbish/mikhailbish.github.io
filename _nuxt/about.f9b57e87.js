3b612b0c2a34e413cfab1ae00812cd6c25afdf85
Mikhail Butuzov <mikhailbish@gmail.com>
2023-04-05 00:31:52 +0300

    some other commit

---
+++ _nuxt/about.f9b57e87.js
@@ -0,0 +1,1 @@
+import{a as r}from"./entry.b859ccac.js";const t={};function c(e,n){return null}const _=r(t,[["render",c]]);export{_ as default};
--- about/_payload.js
+++ about/_payload.js
@@ -1,1 +1,1 @@
-export default {data:{},prerenderedAt:1680643643170}
+export default {data:{},prerenderedAt:1680643864389}
